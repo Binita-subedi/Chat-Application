fdb03e4e435416d7de8afd1e0f3c9f42fb14b389
Binita-subedi <subedibinita94@gmail.com>
2022-04-03 15:49:10 +0545

    Integrating frontend with backend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Route path='/' />
+      <Route path='/chats' />
+    </div>
+  );
+}
+
+export default App;
